4f07f66c26aed0653b5a8e7f70b9ffd90540aac4
QinYong <327302008@qq.com>
2015-07-16 13:01:29 +0800

    change class10_4

--- class10_4.py
+++ class10_4.py
@@ -58,3 +58,4 @@ json.dumps(d)
 #dumps()方法返回一个str，内容就是标准的JSON。类似的，dump()方法可以直接把JSON写入一个file-like Object。
 
 #要把JSON反序列化为Python对象，用loads()或者对应的load()方法，前者把JSON的字符串反序列化，后者从file-like Object中读取字符串并反序列化：
+#============================================================================
